3c29b55b67456c808b974f236f3ff93e8a795d69
Ville Tanttu <ville.tanttu@cs.helsinki.fi>
2016-02-07 21:32:26 +0200

    Viikko 3

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -3,6 +3,17 @@ class Brewery < ActiveRecord::Base
   
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
+
+  validate :year_cannot_be_in_the_future
+  validates :name, presence: true
+  validates :year, numericality: { greater_than_or_equal_to: 1042,
+                                   only_integer: true }
+  
+  def year_cannot_be_in_the_future
+    if year > Time.now.year
+      errors.add(:year, "can't be in the future")
+    end
+  end
   
   def print_report
     puts name
